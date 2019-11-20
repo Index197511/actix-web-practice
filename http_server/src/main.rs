f6a458605467cae29d56d8ea75b474e6d6fa3f9b
Index197511 <hiro.saten@gmail.com>
2019-11-20 19:19:08 +0900

    return http response

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,23 +1,24 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-
-fn index(_req: HttpRequest) -> impl Responder {
-    "Welcome!"
+use actix_web::{get, post, App, HttpServer, HttpResponse, Responder};
+ 
+#[get("/get")]
+fn get() -> impl Responder {
+    HttpResponse::Ok().body("GET")
+}
+ 
+#[post("/post")]
+fn post(a: String) -> (){
+    println!("POST");
+    println!("{}", a);
 }
 
-pub fn main() {
-    // load ssl keys
-    // to create a self-signed temporary cert for testing:
-    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
-    let mut builder =
-        SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
-
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind_ssl("127.0.0.1:8088", builder)
+ 
+fn main() {
+    HttpServer::new(
+        || App::new()
+            .service(get)
+            .service(post)
+        )
+        .bind("127.0.0.1:8088")
         .unwrap()
         .run()
         .unwrap();
